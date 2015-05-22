e4d09cd9ccde43f0e4191d25e32aa7b159069a3e
ramlall <tariqramlall@gmail.com>
2015-05-22 17:22:50 -0400

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+// Program to read the contents of a file, capitalize every letter, and output the contents to a file.
+class ProfDevi
+	{
+		static void Main(string[] args)
+			{
+			StreamWriter sw = new StreamWriter(@"C:\work\FileOutput\output.txt");
+			string text = System.IO.File.ReadAllText(@"C:\work\FileOutput\input.txt"); // Strings are immutable so we can't edit text.
+			StringBuilder newText = new StringBuilder(text);
+			for (int k = 0; k < text.Length; k++)
+				{
+				if (text[k] >= 'a' && text[k] <= 'z') // If the char is a letter from a to z, lowercase.
+					{
+					newText[k] = Char.ToUpper(text[k]);
+					}
+				}
+			sw.WriteLine(newText);
+			sw.Close();
+			}
+	}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # FileOutput
 A quick and simple C# program to show how file output works. 
+
+To test the program, edit the "input.txt" file and write anything you want inside of it. 
+Then build and run the C# code. The output.txt will be the same as the input file but 
+will have every letter capitalized. 
