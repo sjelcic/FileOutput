e20cb11ef93e6313a7a0d8b958b1f3df25730800
ramlall <tariqramlall@gmail.com>
2015-05-25 00:02:29 -0400

    You must now use the command line to enter file name.
    Do this using the Developer Command Prompt from Visual Studio 2013 tools folder.
    I also changed the variable names to be all lowercase.
    And I added some failsafes in case you don't actually enter a file name or if the file doesn't exist.
    Also changed an if statement so the integer representing the char we're reading doesn't get cast to a char for the comparison.

--- Program.cs
+++ Program.cs
@@ -1,28 +1,59 @@
-﻿// Program to read the contents of a file character by character. 
-// If the char is lowercase then we'll capitalize it.
+﻿// Program to read the contents of a file character by character and capitalize all the letters.
+// To use this program you must use the command line and provide the file name. 
+// Build the program using the Developer Command Prompt and using that terminal you can run the .exe generated and enter the argument.
+// More instructions here: https://msdn.microsoft.com/en-us/library/cb20e19t.aspx
 using System;
 using System.Text;
 using System.IO;
 
 class ProfDevi
 	{
-	static void Main(string[] args)
+	// Function to read characters in a file, capitalize the 
+	static void CapitalizeToFile(string path)
 		{
-		FileInfo MyFile = new FileInfo(@"c:\work\FileOutput\input.txt");      //Create a FileInfo instance representing an existing text file.
-		StreamReader sr = MyFile.OpenText();                                  //Instantiate a StreamReader to read from the text file.
+		FileInfo myFile = new FileInfo(path);      //Create a FileInfo instance representing an existing text file.
+		StreamReader sr = myFile.OpenText();                                  //Instantiate a StreamReader to read from the text file.
 		StreamWriter sw = new StreamWriter(@"C:\work\FileOutput\output.txt"); //Setup file to write too.
-
+	
 		while (!sr.EndOfStream)												  // Scrolling through file until end.
 			{
-			int CharAsInt = sr.Read();										  // Reads a single char.
-
-			if ((Char)CharAsInt >= 'a' && (Char)CharAsInt <= 'z')			  // If the char is a lowercase letter from a to z.
-				{ CharAsInt = CharAsInt + 'A' - 'a'; }
-
-			sw.Write((Char)CharAsInt);										  // Write the character to the output file.
-			}
+			int charAsInt = sr.Read();										  // Reads a single char.
 		
+			if (charAsInt >= 'a' && charAsInt <= 'z')			  // If the char is a lowercase letter from a to z.
+				{ charAsInt = charAsInt + 'A' - 'a'; }
+			
+			sw.Write((Char)charAsInt);										  // Write the character to the output file.
+			}
+
 		sr.Close();
 		sw.Close();
 		}
+
+	static void Main(string[] args)
+		{
+		if (args.Length == 0) // Did user provide an argument?
+			{
+			System.Console.WriteLine("Please enter a path like /work/fileoutput/input.txt");
+			}
+		else if (!File.Exists(args[0])) // Does that file actually exist?
+			{ System.Console.WriteLine("That file doesn't exist."); }
+		else
+			{ CapitalizeToFile(args[0]); } // If there is an argument capitalize the letters.
+		
+		}
 	}
+
+///////////////////////////////////////////////
+/// Notes:
+/// Chars are unicode and are 16 bits wide. 
+/// A stream reader/writer hides the details of an ASCII vs. Unicode file.
+/// THere may be problems in the code with bite ordering.
+/// 
+
+
+/// Changes to make:
+/// Make file get put in from the command line (args array) DONE
+/// Don't name local variables with upper case. Upper case is for class names DONE
+/// Don't cast the integer into char for int statement. Upcast to integer. DONE
+/// Write code for cases where the file doesn't exist. DONE
+/// Don't use "@". "Use / chars instead. To meet cross platform Dev, use /work/... Don't use c:." DONE
