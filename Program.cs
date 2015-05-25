19ad38b0977a42783568b634697ca776757993b0
ramlall <tariqramlall@gmail.com>
2015-05-24 20:36:19 -0400

    Changed program to read the file char by char instead of one big string.
    I should note that (lol) currently the program doesn't actually do the capitalization. If you uncomment lines 22 and 23 in Program.cs, the program should theoretically check to see if the character is a lowercase letter and then add enough value to it to make it uppercase. That is to say:
    
    Uppercase = Lowercase + 'A' - 'a'
    
    But there's an error where C# doesn't like the addition of a char to anything. The error specifically says 'Cannot implictly convert char to int' in line 23.

--- Program.cs
+++ Program.cs
@@ -1,23 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// Program to read the contents of a file character by character. 
+// If the char is lowercase then we'll capitalize it.
+using System;
 using System.Text;
 using System.IO;
-// Program to read the contents of a file, capitalize every letter, and output the contents to a file.
+
 class ProfDevi
 	{
-		static void Main(string[] args)
+	static void Main(string[] args)
+		{
+		FileInfo MyFile = new FileInfo(@"c:\work\FileOutput\input.txt"); //Create a FileInfo instance representing an existing text file.
+		StreamReader sr = MyFile.OpenText(); //Instantiate a StreamReader to read from the text file.
+		StreamWriter sw = new StreamWriter(@"C:\work\FileOutput\output.txt"); // Write to a file.
+
+		while (!sr.EndOfStream) // Scrolling through file until end.
 			{
-			StreamWriter sw = new StreamWriter(@"C:\work\FileOutput\output.txt");
-			string text = System.IO.File.ReadAllText(@"C:\work\FileOutput\input.txt"); // Strings are immutable so we can't edit text.
-			StringBuilder newText = new StringBuilder(text);
-			for (int k = 0; k < text.Length; k++)
-				{
-				if (text[k] >= 'a' && text[k] <= 'z') // If the char is a letter from a to z, lowercase.
-					{
-					newText[k] = Char.ToUpper(text[k]);
-					}
-				}
-			sw.WriteLine(newText);
-			sw.Close();
+			int Temp = sr.Read(); // Reads a single char.
+
+
+			Char WriteToFile = (Char)Temp; // Convert the int to a char.
+
+			//if (WriteToFile >= 'a' && WriteToFile <= 'z') // If the char is a lowercase letter from a to z.
+				//{ WriteToFile = WriteToFile + 'A' - 'a'; }
+
+			sw.Write(WriteToFile);
 			}
+		
+		sr.Close();
+		sw.Close();
+		}
 	}
