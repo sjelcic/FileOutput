fd4c88e3ffd7075c489f97cedece7e12598ca409
ramlall <tariqramlall@gmail.com>
2015-05-26 20:34:14 -0400

    Made a class to hold all the functions. I/O is handled by try-catch.
    You now have to specify both the input file and the output file as command line arguments. The output file must already exist or you catch an error.
    
    I know Devi said to 'clean up' the output file if the I/O fails but...like...nothing actually changes unless both files exist soooooo...not really necessary.

---
+++ FileOps.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+
+public class FileOps
+	{
+	///////////////////////////////////////////////////////////////
+	public FileOps()
+		{
+		// Blank constructor
+		}
+
+	///////////////////////////////////////////////////////////////
+	~FileOps()
+		{
+		// Might as well make a blank destructor too.
+		}
+
+	///////////////////////////////////////////////////////////////
+	// A function to check if the file input exists. 
+	public static bool FileWorks(string file)
+		{
+		// If that doesn't happen then I guess it will catch any other Exception.
+		try										// I guess try will try to open the file. 
+			{
+			StreamReader sr = File.OpenText(file);
+			sr.Close();
+			}
+		catch (Exception e)						// If that doesn't happen then I guess it will catch any other Exception.
+			{
+			Console.WriteLine("The {0} doesn't exist. Please create it or enter a correct path to it.", file);
+			Console.WriteLine("An error occurred: '{0}'", e);
+			return false;
+			}
+		
+		return true;
+		}
+
+	///////////////////////////////////////////////////////////////
+	//Checks if a char is a lowercase letter.
+	static bool CheckIfLowercase(int character)
+		{
+		if (character >= 'a' && character <= 'z')	// If the char is a lowercase letter from a to z.
+			{ return true; }
+		else
+			{ return false; }
+		}
+
+
+	///////////////////////////////////////////////////////////////
+	//Converts a lowercase letter to uppercase.
+	static int ToUpperCase(int character)
+		{
+		character = character + 'A' - 'a';
+		return character;
+		}
+
+	///////////////////////////////////////////////////////////////
+	// Function to capitalize letters of an input file to an output file.
+	public static void CapitalizeToFile(string inputFile, string outputFile)
+		{
+		FileInfo myFile = new FileInfo(inputFile);		//Create a FileInfo instance representing an existing text file.
+		StreamReader sr = myFile.OpenText();            //Instantiate a StreamReader to read from the text file.
+		StreamWriter sw = new StreamWriter(outputFile); //Setup file to write too.
+
+		while (!sr.EndOfStream)							// Scrolling through file until end.
+			{
+			int charAsInt = sr.Read();					// Reads a single char.
+
+			if (CheckIfLowercase(charAsInt) == true)	// If the char is a lowercase letter from a to z.
+					{ charAsInt = ToUpperCase(charAsInt); }
+
+			sw.Write((Char)charAsInt);					// Write the character to the output file.
+			}
+
+		sr.Close();
+		sw.Close();
+		}
+	///////////////////////////////////////////////////////////////
+	}
---
+++ MainStartup.cs
@@ -0,0 +1,24 @@
+﻿// Program to read the contents of a file character by character and capitalize all the letters.
+using System;
+using System.IO;
+
+class ProfDevi
+	{
+	static void Main(string[] args)
+		{
+		if (args.Length < 2) // Did user provide an argument?
+			{ System.Console.WriteLine("Please enter both an input file and an output file."); }
+
+		if (FileOps.FileWorks(args[0]) == true && FileOps.FileWorks(args[1]) == true) // If input and output file exist.
+			{ FileOps.CapitalizeToFile(args[0], args[1]); } // Capitalize the letters. 
+		}
+	} 
+
+///If the input file did not exist, look up exceptions and try catch blocks. DONE
+///If file input and output has problems, you will throw exceptions. DONE
+///Create a class in a seperate file called FileOps. Leave the constructor blank. DONE
+///Move the code over into FileOps class as an object member. DONE
+///Rename the function as bool ToUpperCase(string infile, string outfile). DONE 
+///Place try catch blocks are io ops to catch mistakes. Your function will return true if successful. DONE
+///If it fails it should return false. Cleanup the output file if your code fails. DONE
+///You don't need to go to console to pass arguments. You can do it on the project properties debug option. DONE
